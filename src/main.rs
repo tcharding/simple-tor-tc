951f31ed55821c3684ff1ceebc19aedf1c5a17db
Tobin C. Harding <tobin@coblox.tech>
2020-06-20 13:44:41 +1000

    Add skeleton tor connection
    Add a function to assert we can connect to a local Tor instance.  Add
    three todos, one for each known Rust TC library.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{bail, Result};
+
+// const TC_PORT: u16 = 9051;
+
+fn main() -> Result<()> {
+    assert_can_connect_to_tor()?;
+
+    Ok(())
+}
+
+/// Asserts we can make a TCP connection to a local tor instance on TC_PORT.
+fn assert_can_connect_to_tor() -> Result<()> {
+    // TODO: Connect using tor_control
+    // TODO: Connect using tor-client
+    // TODO: Connect using torut
+
+    bail!("failed to connect to local Tor instance")
 }
