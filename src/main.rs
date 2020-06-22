70499e63252a5c4d602047a66931801faae14ee3
Tobin C. Harding <tobin@coblox.tech>
2020-06-22 10:37:45 +1000

    Add event handler

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
 use anyhow::{bail, Result};
-use tokio::net::TcpStream;
-use torut::control::{UnauthenticatedConn, AuthenticatedConn};
+use torut::control::{UnauthenticatedConn};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let stream = simple_tor_tc::connect().await?;
-    let mut conn = UnauthenticatedConn::new(stream);
-    let info = match conn.load_protocol_info().await {
+
+    let mut utc = UnauthenticatedConn::new(stream);
+
+    let info = match utc.load_protocol_info().await {
         Ok(info) => info,
         Err(_) => bail!("failed to load protocol info from Tor")
     };
+    let ad = info.make_auth_data()?.expect("failed to make auth data");
 
-    let auth_data = info.make_auth_data()?.expect("failed to make auth data");
-    if conn.authenticate(&auth_data).await.is_err() {
+    if utc.authenticate(&ad).await.is_err() {
         bail!("failed to authenticate with Tor")
     }
+    let mut ac = utc.into_authenticated().await;
 
-    let _conn: AuthenticatedConn<TcpStream, ()> = conn.into_authenticated().await;
+    ac.set_async_event_handler(Some(|_| {
+        async move { Ok(()) }
+    }));
+
+    ac.take_ownership().await.unwrap();
 
     Ok(())
 }
